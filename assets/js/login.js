254c736b8dc441f3340b0b6c2593f810c9dd34c5
itlele <itlele@git.cn>
2022-08-14 17:40:06 +0800

    完成了登录和注册的功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,74 +1,71 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
-    $('.login-box').hide()
-    $('.reg-box').show()
+$(function () {
+  $('#link_reg').on('click', function () {
+    $('#reg').show()
+    $('#login').hide()
   })
-
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
-    $('.login-box').show()
-    $('.reg-box').hide()
+  $('#link_login').on('click', function () {
+    $('#login').show()
+    $('#reg').hide()
   })
-
-  // 从 layui 中获取 form 对象
+  // 从layui 中获取form对象
   var form = layui.form
-  var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
+  // 从layui 中获取layer
+  let layer = layui.layer
+  // 使用 form.verify() 函数自定义校验规则
   form.verify({
-    // 自定义了一个叫做 pwd 校验规则
+    // 自定义一个pwd 校验规则
     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
-      var pwd = $('.reg-box [name=password]').val()
+
+    repwd: function (value) {
+      // 我们拿到的是确认密码框的值
+      // 需要拿到的是密码框的值
+      // 我们进行等值的判断
+      // 如果失败，return提示错误消息
+      var pwd = $('#upwd').val()
       if (pwd !== value) {
-        return '两次密码不一致！'
+        return '两次密码不一致'
       }
     }
   })
 
+  let data = {
+    username: $('#form_reg input[name=username]').val(),
+    password: $('#form_reg input[name=password]').val()
+  }
   // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
+  $('#form_reg').on('submit', function (e) {
+    //  1.阻止表单默认提交行为
     e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
-    }
-    $.post('/api/reguser', data, function(res) {
+    //  在发起ajax中的 post 请求
+    $.post('/api/reguser', data, function (res) {
       if (res.status !== 0) {
         return layer.msg(res.message)
       }
-      layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
+      layer.msg('注册成功,请登录！')
+      // 当注册成功跳转到登录页面 .click()
       $('#link_login').click()
     })
+
   })
 
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
-    // 阻止默认提交行为
+  $('.layui-form').submit(function (e) {
     e.preventDefault()
     $.ajax({
-      url: '/api/login',
       method: 'POST',
-      // 快速获取表单中的数据
+      url: '/api/login',
       data: $(this).serialize(),
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
-          return layer.msg('登录失败！')
+          return layer.msg('登入失败！')
         }
-        layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
+        layer.msg('登入成功！')
+        // console.log(res.token)
+        // 将登入成功得到的 token 字符串保存到本地localStorage
         localStorage.setItem('token', res.token)
-        // 跳转到后台主页
+        //跳转到后台主页
         location.href = '/index.html'
       }
     })
   })
+
 })
